f347eeece27238602368c504c98c0eabf2465311
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-03-07 16:46:12 +0000

    Website update (1)

---
+++ js/pele.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Santiago All rights reserved
+//
+// Created by: Santiago
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById("pele").innerHTML = "<p>Thank you for Sbmiting!</p>"
+}
